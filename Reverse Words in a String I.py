5a6ad4cef4c9ff08c0b2a770a887903d680f9582
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2025-06-17 23:02:20 +0530

    optimization

--- Reverse Words in a String I.py
+++ Reverse Words in a String I.py
@@ -1,3 +1,8 @@
+# alternatively 
+# class Solution:
+   # def reverseWords(self, s: str) -> str:
+       # return " ".join(s.split()[::-1])
+
 class Solution:
     def reverseWords(self, s: str) -> str:
         liststr =  s.split()
