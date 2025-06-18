76b992468204dfbbaef8b7a3745b9c2ad4ddf48c
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2025-06-18 13:36:07 +0530

    product except self (array)

--- Product of Array Except Self.py
+++ Product of Array Except Self.py
@@ -1,3 +1,19 @@
 class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
-        
+    def productExceptSelf(self, nums: list[int]) -> list[int]:
+        n = len(nums)
+        output = [1] * n
+        left = 1
+        for i in range(n):
+            output[i] = left
+            left *= nums[i]
+        right = 1
+        for i in range(n - 1, -1, -1):
+            output[i] *= right
+            right *= nums[i]
+        return output
+    
+sol = Solution()
+print(sol.productExceptSelf([1, 2, 3, 4]))      
+print(sol.productExceptSelf([-1, 1, 0, -3, 3])) 
+print(sol.productExceptSelf([0, 0]))            
+
