1fcc7cc063e7521acf36fc341833dfbc702b15a1
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2025-06-18 20:06:51 +0530

    updation

--- practice string.py
+++ practice string.py
@@ -1,8 +1,4 @@
 str1 = "ABABAB"
 str2 = "ABAB"
-
-for i in range(len(str1)):
-    candidate = str1[i:]                      
-    if len(str1) % len(candidate) == 0:       
-        if candidate * (len(str1) // len(candidate)) == str1:
-            print(candidate)
+nums = [2,1,5,0,4,6]
+print(nums[0])
