41a88a7b995fb99aaf5cb20fe294ed96493ba97f
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2025-06-20 19:14:44 +0530

    valid parentheses

--- practice string.py
+++ practice string.py
@@ -1,7 +1,15 @@
 str1 = "ABABAB"
 str2 = "ABAB"
 nums = [2,1,5,0,4,6]
-print(nums[0])
 iop = "".join(map(str ,nums)) 
-print(iop)
-print(iop+str1)
+paren = {')':'(','}':'{',']':'['}
+empty= []
+def stacky()
+for i in str1:
+    if i in empty:
+        if empty and empty[-1] == paren:
+            stack.pop()
+        else: return False
+    else: 
+        empty.append(i)
+    return True if not stack else False 
