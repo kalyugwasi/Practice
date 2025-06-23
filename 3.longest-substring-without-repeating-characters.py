1b296ec3284e439b49b68c98359f637800d4236b
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2025-06-23 13:51:51 +0530

    longest sustring without rep

--- 3.longest-substring-without-repeating-characters.py
+++ 3.longest-substring-without-repeating-characters.py
@@ -7,22 +7,15 @@
 # @lc code=start
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        length = 0
-        maxstr = []
-        l , r = 0 , 1
-        while l < len(s):
-            maxstr.append(list(s[l]))
-            if list(s[r]) not in maxstr:
-                maxstr.append(list(s[r]))
-                r += 1
-                length = max(length , len(maxstr))
-            else:
-                maxstr.append(list(s[r]))
-                maxstr.pop(0)
-                l = r+1
-        print(length)
-sol = Solution()
-print(sol.lengthOfLongestSubstring("abcabcbb")) 
-        
+        charSet = set()
+        l = 0
+        result = 0
+        for r in range(len(s)):
+            while s[r] in charSet:
+                charSet.remove(s[l])
+                l += 1
+            charSet.add(s[r])
+            result = max(result, r-l +1)
+        return result
 # @lc code=end
 
