b93a8296b30bf207cd7819eca253b5979d0aa375
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2025-06-23 23:40:36 +0530

    part

---
+++ 424.longest-repeating-character-replacement.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=424 lang=python3
+#
+# [424] Longest Repeating Character Replacement
+#
+
+# @lc code=start
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        res , l = 0 , 0
+        for r in range(len(s)):
+            
+
+sol = Solution()
+print(sol.characterReplacement("ABAB", 2))
+        
+# @lc code=end
+
