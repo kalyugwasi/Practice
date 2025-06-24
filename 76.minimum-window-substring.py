340d3a0951e05ec94945c9fd9aa4f0edfbe4aa98
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2025-06-24 08:25:09 +0530

    sliding win hard

--- 76.minimum-window-substring.py
+++ 76.minimum-window-substring.py
@@ -7,6 +7,28 @@
 # @lc code=start
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        
+        m, n = len(s) , len(t)
+        have , need = 0 , n
+        window , tchar = {} , {}
+        res , resl = [-1,-1], float("infinity")
+        l = 0
+        for char in t:
+            tchar[char] = tchar.get(char,0) + 1
+        if t == "": return ""
+        for r in range(len(s)):
+            c = s[r]
+            window[c] = window.get(c,0) + 1
+            if c in tchar and window[c] <= tchar[c]:
+                have += 1
+            while have == need:
+                if (r-l+1) < resl:
+                    res=[l,r]
+                    resl = r-l+1
+                window[s[l]] -= 1
+                if s[l] in tchar and window[s[l]] < tchar[s[l]]:
+                    have -= 1
+                l += 1
+        l,r = res
+        return s[l:r+1] if  resl != float("infinity") else "" 
 # @lc code=end
 
