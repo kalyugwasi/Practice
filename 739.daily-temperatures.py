629f3a71f867473751698933bf56d14df3f9f3c5
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2025-06-24 16:18:06 +0530

    daily temp.

--- 739.daily-temperatures.py
+++ 739.daily-temperatures.py
@@ -8,11 +8,12 @@
 class Solution:
     def dailyTemperatures(self, temperatures: list[int]) -> list[int]:
         res = [0] * len(temperatures)
-        for i in range(len(temperatures)):
-            for j in range(i+1 ,len(temperatures)):
-                if temperatures[j] > temperatures[i]:
-                    res[i] = (j-i)
-                    break
+        stack = []
+        for i,temp in enumerate(temperatures):
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                prev_index = stack.pop()
+                res[prev_index] = i - prev_index
+            stack.append(i)
         return res
 
 # @lc code=end
