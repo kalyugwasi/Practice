94a96fdd84329d1abbb3dfad9818aa0c5997114a
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2025-06-27 22:07:01 +0530

    rev LL

--- 206.reverse-linked-list.py
+++ 206.reverse-linked-list.py
@@ -12,6 +12,14 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+         curr , prev = head , None
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        return prev
+
         
 # @lc code=end
 
