e2e3920c4b3d2081951e2fcbcc7cfbc2cae25680
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2025-06-28 14:56:47 +0530

    LL cycle 1

--- practice string.py
+++ practice string.py
@@ -1,7 +1,10 @@
-str1 = "ABABAB"
-str2 = "ABAB"
-nums = [2,1,5,0,4,6]
-iop = "".join(map(str ,nums)) 
-paren = {')':'(','}':'{',']':'['}
-empty= []
-print(list(str1[0]))
+# Given dictionary
+student_grades = {"Alice": 85, "Bob": 72, "Charlie": 90, "David": 65, "Eva": 88, "John": 45}
+
+# Complete the code 
+import sys
+t = "Bob"
+if t in student_grades.keys():
+    a = student_grades[t]
+    print(a)
+else: print("Not Found")
