b460b33deea6897729558645450bf30ceabca81b
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2025-07-03 22:55:07 +0530

    Refactor lowestCommonAncestor function to use recursive approach for finding the lowest common ancestor in a binary tree

--- 236.lowest-common-ancestor-of-a-binary-tree.py
+++ 236.lowest-common-ancestor-of-a-binary-tree.py
@@ -14,13 +14,14 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        cur = root
-        while cur:
-            if p.val > cur.val and q.val > cur.val:
-                cur = cur.right
-            elif p.val < cur.val and q.val < cur.val:
-                cur = cur.left
-            else: return cur 
+        if not root or root == p or root == q:
+            return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        if left and right:
+            return root
+        return left if left else right
+
 
         
 # @lc code=end
