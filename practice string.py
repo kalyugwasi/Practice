90c1123b27731c8f119fbf35dfab704f90d0485a
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2025-07-07 09:56:01 +0530

    Refactor palindrome check logic and implement kth deletable function

--- practice string.py
+++ practice string.py
@@ -1,10 +1,42 @@
-# Given dictionary
-student_grades = {"Alice": 85, "Bob": 72, "Charlie": 90, "David": 65, "Eva": 88, "John": 45}
-
-# Complete the code 
 import sys
-t = "Bob"
-if t in student_grades.keys():
-    a = student_grades[t]
-    print(a)
-else: print("Not Found")
+input = sys.stdin.readline
+
+def is_kth_deletable(arr, k, bad_val):
+    l = 0
+    r = len(arr) - 1
+    while l < r:
+        if arr[l] == arr[r]:
+            l += 1
+            r -= 1
+        else:
+            if arr[l] == bad_val:
+                l += 1
+            elif arr[r] == bad_val:
+                r -= 1
+            else:
+                return False
+    return True
+
+def solve():
+    t = int(input())
+    for _ in range(t):
+        n, k = map(int, input().split())
+        a = list(map(int, input().split()))
+
+        l = 0
+        r = n - 1
+        while l < r and a[l] == a[r]:
+            l += 1
+            r -= 1
+        
+        if l >= r:
+            print("YES")
+            continue
+
+        # Try removing a[l] or a[r] completely to make it a palindrome
+        if is_kth_deletable(a[l:r+1], k, a[l]) or is_kth_deletable(a[l:r+1], k, a[r]):
+            print("YES")
+        else:
+            print("NO")
+
+solve()
