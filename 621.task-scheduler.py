8c9bb4d1e789d317005d0c2ca341a0ddcef9bd11
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2025-07-13 00:06:38 +0530

    ini

---
+++ 621.task-scheduler.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=621 lang=python3
+#
+# [621] Task Scheduler
+#
+
+# @lc code=start
+
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        major = collections.Counter(tasks)
+        if len(major.items()) <= n:
+        else:
+            
+
+
+
+        
+# @lc code=end
+
