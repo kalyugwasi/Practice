ed831f0bf36509ee5d63c1af15839c888e7334f9
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2025-07-18 15:38:31 +0530

    comb. ph no

---
+++ 17.letter-combinations-of-a-phone-number.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=17 lang=python3
+#
+# [17] Letter Combinations of a Phone Number
+#
+
+# @lc code=start
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+        res = []
+        maps = {'2': "abc", '3': "def", '4': "ghi", '5': "jkl",
+            '6': "mno", '7': "pqrs", '8': "tuv", '9': "wxyz"}
+        def backtrack(index,path):
+            if index == len(digits):
+                res.append(path)
+                return
+            possible = maps[digits[index]]
+            for char in possible:
+                backtrack(index+1,path+char)
+        
+        backtrack(0,"")
+        return res
+        
+        
+        
+# @lc code=end
+
