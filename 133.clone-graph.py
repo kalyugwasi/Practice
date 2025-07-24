1c308b754b465b4094885ebd44dcc9f52d757b64
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2025-07-24 19:54:31 +0530

    clone graph

---
+++ 133.clone-graph.py
@@ -0,0 +1,31 @@
+#
+# @lc app=leetcode id=133 lang=python3
+#
+# [133] Clone Graph
+#
+
+# @lc code=start
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        res = {}
+        def dfs(node):
+            if node in res:
+                return res[node]
+            copy = Node(node.val)
+            res[node] = copy
+            for n in node.neighbors:
+                copy.neighbors.append(dfs(n))
+            return copy
+        return dfs(node) if node else None
+        
+# @lc code=end
+
