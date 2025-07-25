383953bc6399fc245d055470c260adc487007baf
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2025-07-25 20:04:43 +0530

    rotting orng

--- 994.rotting-oranges.py
+++ 994.rotting-oranges.py
@@ -7,37 +7,30 @@
 # @lc code=start
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
+        q=deque()
+        time,fresh = 0,0
         rows,cols = len(grid),len(grid[0])
-        visit = set()
-        q = deque()
-
-        def addRoom(r,c):
-            if (r<0 or r>=rows or c<0 or c>=cols or (r,c) in visit or grid[r][c] == 0):
-                return
-            visit.add((r,c))
-            q.append([r,c])   
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c]==2:
-                    q.append([r,c])
-                    visit.add((r,c))
-        dist= 3
-        res = 0
-        while q:
-            for i in range(len(q)):
-                r,c=q.popleft()
-                grid[r][c] = dist
-                addRoom(r+1,c)
-                addRoom(r-1,c)
-                addRoom(r,c+1)
-                addRoom(r,c-1)
-            dist += 1
-            res = max(grid[r][c],res)
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 1:
-                    return -1
-        return res-3 if res > 3 else 0
+                    fresh += 1
+                if grid[r][c] == 2:
+                    q.append([r,c])
+        directions = [0,1],[0,-1],[1,0],[-1,0]
+        while q and fresh >0:
+            for i in range(len(q)):
+                r,c=q.popleft()
+                for dr,dc in directions:
+                    row , col = dr+r , dc+c
+                    if (row<0 or row>=rows or col<0 or col>=cols or grid[row][col] != 1):
+                        continue
+                    grid[row][col] = 2
+                    q.append([row,col])
+                    fresh -= 1
+            time += 1
+        return time if fresh == 0 else -1
+
+
         
 # @lc code=end
 
