e06abd994fcd8caf18145963debe74541409db08
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2025-07-27 18:02:21 +0530

    schedule 1

--- 207.course-schedule.py
+++ 207.course-schedule.py
@@ -7,6 +7,24 @@
 # @lc code=start
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        mapy = {i:[] for i in range(numCourses)}
+        for crs,pre in prerequisites:
+            mapy[crs].append(pre)
+        visitSet = set()
+        def dfs(crs):
+            if crs in visitSet:
+                return False
+            if mapy[crs] == []:
+                return True
+            visitSet.add(crs)
+            for pre in mapy[crs]:
+                if not dfs(pre): return False
+            visitSet.remove(crs)
+            mapy[crs] = []
+            return True
+        for crs in range(numCourses):
+            if not dfs(crs) : return False
+        return True
         
 # @lc code=end
 
