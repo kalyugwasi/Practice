28b7b0ab859e1ebf58edf80f9d235d9e924befd9
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2025-08-07 15:33:59 +0530

    rev sort CP

--- reversort.py
+++ reversort.py
@@ -1,14 +1,14 @@
 import sys
-o = sys.stdin.readline
-t = int(o())
-for h in range(t):
-    n = int(o())
-    L = list(map(int,o().split()))
-    def reversort(L:list)->int:
-        cost = 0
-        for i in range(len(L)-1):
-            j = i + L[i:].index(min(L[i:]))
-            L[i:j+1] = reversed(L[i:j+1])
-            cost += (j-i+1)
-        return cost
-    print("#Case", h+1,":",reversort(L))
+inp = sys.stdin.readline
+
+def reversort(L: list) -> int:
+    cost = 0
+    for i in range(len(L)-1):
+        j = i + L[i:].index(min(L[i:]))
+        L[i:j+1] = reversed(L[i:j+1])
+        cost += (j - i + 1)
+    return cost
+
+for _ in range(int(inp())):
+    n,L = int(inp()),list(map(int,inp().split()))
+    print("Case #",_+1,":",reversort(L))
