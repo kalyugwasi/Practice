319fa6ee1c48e108afb7d11a30ca88487779252d
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2025-08-12 22:33:05 +0530

    robbing

---
+++ 213.house-robber-ii.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=213 lang=python3
+#
+# [213] House Robber II
+#
+
+# @lc code=start
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        def helper(nums):
+            rob1,rob2=0,0
+            for n in nums:
+                temp = max(rob1 + n ,rob2)
+                rob1 = rob2
+                rob2 = temp
+            return rob2
+        return max(nums[0],helper(nums[1:]),helper(nums[:-1]))
+        
+# @lc code=end
+
