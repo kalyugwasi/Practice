ea58ade05193f7474ac0da38007d8f1f71bc0c6f
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2025-08-23 20:16:53 +0530

    target sum

---
+++ 494.target-sum.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=494 lang=python3
+#
+# [494] Target Sum
+#
+
+# @lc code=start
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        dp = {}
+        def bt(i,res):
+            if i==len(nums):
+                return 1 if res == target else 0
+            if (i,res) in dp:
+                return dp[(i,res)]
+            dp[(i,res)] = bt(i+1,res+nums[i]) + bt(i+1,res-nums[i])
+            return dp[(i,res)]
+        return bt(0,0)
+        
+# @lc code=end
+
