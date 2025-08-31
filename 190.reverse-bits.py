6b57a3e86876f6e267baef2d4d2d5e33299bd880
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2025-08-31 23:26:08 +0530

    rev bits

---
+++ 190.reverse-bits.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=190 lang=python3
+#
+# [190] Reverse Bits
+#
+
+# @lc code=start
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        res = 0
+        for _ in range(32):
+            res = (res << 1) | (n & 1)
+            n >>= 1
+        return res
+# @lc code=end
+
