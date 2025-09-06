d5080b572da484e28f1a180663b68610c57707fc
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2025-09-06 21:40:33 +0530

    setzero

--- 73.set-matrix-zeroes.py
+++ 73.set-matrix-zeroes.py
@@ -5,23 +5,39 @@
 #
 
 # @lc code=start
+from typing import List
+
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
         row, col = len(matrix), len(matrix[0])
-        zrow,zcol = [],[]
+        rowZero = False
+        colZero = False
+        for c in range(col):
+            if matrix[0][c] == 0:
+                rowZero = True
+                break
         for r in range(row):
-            for c in range(col):
+            if matrix[r][0] == 0:
+                colZero = True
+                break
+        for r in range(1, row):
+            for c in range(1, col):
                 if matrix[r][c] == 0:
-                    zrow.append(r)
-                    zcol.append(c)
-        for r in set(zrow):
+                    matrix[r][0] = 0
+                    matrix[0][c] = 0
+        for r in range(1, row):
+            for c in range(1, col):
+                if matrix[r][0] == 0 or matrix[0][c] == 0:
+                    matrix[r][c] = 0
+        if rowZero:
             for c in range(col):
-                matrix[r][c] = 0
-        for r in range(row):
-            for c in set(zcol):
-                matrix[r][c] = 0
+                matrix[0][c] = 0
+        if colZero:
+            for r in range(row):
+                matrix[r][0] = 0
+
 # @lc code=end
 
