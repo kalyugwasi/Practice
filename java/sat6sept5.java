d076e1bfd8ed3f260af7b4ade9e5827bdd09693c
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2025-09-06 09:36:04 +0530

    constructiors

---
+++ java/sat6sept5.java
@@ -0,0 +1,18 @@
+class Test{
+    Test(int x,int y)
+    {
+        System.out.print(x+y);
+    }
+    Test(int x)
+    {
+        System.out.print(x);
+    }
+}
+class sat6sept5
+{
+    public static void main(String args[])
+    {
+        Test t=new Test(20,90);
+        Test t1=new Test(75)
+    }    
+}
