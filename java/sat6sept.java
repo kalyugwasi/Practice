c123601124e41229d8094c940bfd2299cf0a84a8
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2025-09-06 08:44:59 +0530

    calling form another class

---
+++ java/sat6sept.java
@@ -0,0 +1,17 @@
+class Test
+{
+    int a;
+    void show()
+    {
+        System.out.println(a);
+    }
+}
+
+class sat6sept
+{
+    public static void main(String args[])
+    {
+        Test t=new Test();
+        t.show();
+    }
+}
