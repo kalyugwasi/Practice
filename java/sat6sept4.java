673f717386e7df7b76f3c09862efe1916f9978a8
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2025-09-06 09:11:13 +0530

    default constructor with same name

---
+++ java/sat6sept4.java
@@ -0,0 +1,14 @@
+class Test{
+    Test()
+    {
+        System.out.print("My Cons");
+    }
+}
+
+class sat6sept4 
+{
+    public static void main(String args[])
+    {
+        Test t=new Test();
+    }    
+}
