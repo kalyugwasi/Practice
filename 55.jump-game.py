3f8ca9ec9eb5764f04ea7b812ec0329a0961a1ba
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2025-09-12 23:36:49 +0530

    jumping

---
+++ 55.jump-game.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=55 lang=python3
+#
+# [55] Jump Game
+#
+
+# @lc code=start
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        goal = len(nums) - 1
+        for i in range(len(nums)-1,-1,-1):
+            if i + nums[i] >= goal:
+                goal = i
+        if goal == 0:
+            return True
+        else:
+            return False
+        
+# @lc code=end
+
