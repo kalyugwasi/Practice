9dcd0392ba08c83ec8a12b30fd0ea40d0345f9c4
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2025-09-18 20:06:50 +0530

    majority element

---
+++ 169.majority-element.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=169 lang=python3
+#
+# [169] Majority Element
+#
+
+# @lc code=start
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        res = Counter(nums)
+        return max(res,key=res.get)
+# @lc code=end
+
