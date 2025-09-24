72163344e9666398056048f84c5f0cd2bf85172c
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2025-09-25 01:25:37 +0530

    java shit

---
+++ java/calculation.java
@@ -0,0 +1,12 @@
+import Calculation.Calculator;  // import from the package
+
+public class  Calculation {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+
+        System.out.println("Addition: " + calc.add(5, 3));
+        System.out.println("Subtraction: " + calc.subtract(5, 3));
+        System.out.println("Multiplication: " + calc.multiply(5, 3));
+        System.out.println("Division: " + calc.divide(10, 2));
+    }
+}
