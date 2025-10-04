25a0f3406ae03c90e797136a8c0736fcc1abbd2c
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2025-10-04 20:28:48 +0530

    This commit will revise the code with better optimization

--- leetcode/239.sliding-window-maximum.py
+++ leetcode/239.sliding-window-maximum.py
@@ -16,22 +16,25 @@
 #            r += 1
 #        return res
 class Solution:
-    def maxSlidingWindow(self, nums: list[int], k: int) -> list[int]:
-        output = []
-        q = collections.deque()
-        l = r = 0
-        while r < len(nums):
-            while q and nums[q[-1]] < nums[r]:
-                q.pop()    
-            q.append(r)
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        if not nums or k == 0:
+            return []
+        if k > len(nums):
+            return [max(nums)]
+        
+        dq = deque()
+        res = []
+        for i in range(len(nums)):
+            if dq and dq[0] <= i - k:
+                dq.popleft()
+            while dq and nums[dq[-1]] < nums[i]:
+                dq.pop()
+            dq.append(i)
+            if i >= k - 1:
+                res.append(nums[dq[0]])
 
-            if l > q[0]:
-                q.popleft()
-            if (r+1) >= k:
-                output.append(nums[q[0]])
-                l += 1
-            r += 1
-        return output
+        return res
+
 
 
         
