1f86b0f2ea0902414867ab107745cbb4bdb28c13
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2025-10-29 21:49:51 +0530

    quad treee

---
+++ leetcode/427.construct-quad-tree.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode id=427 lang=python3
+#
+# [427] Construct Quad Tree
+#
+
+# @lc code=start
+class Solution:
+    def construct(self, grid: List[List[int]]) -> 'Node':
+        def dfs(n,r,c):
+            allSame = True
+            for i in range(n):
+                for j in range(n):
+                    if grid[r][c] != grid[r+i][c+j]:
+                        allSame = False
+                        break
+            if allSame:
+                return Node(grid[r][c],True,None)
+            n = n//2
+            topleft = dfs(n,r,c)
+            topright = dfs(n,r,c+n)
+            bottomleft = dfs(n,r+n,c)
+            bottomright = dfs(n,r+n,c+n)
+            return Node(0,False,topleft,topright,bottomleft,bottomright)
+        return dfs(len(grid),0,0)
+# @lc code=end
+
