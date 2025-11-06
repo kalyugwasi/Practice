294a0ee2bbe9d572b1ba74e290c4778a6e6d5d00
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2025-11-06 21:39:54 +0530

    Optimization

--- leetcode/1046.last-stone-weight.py
+++ leetcode/1046.last-stone-weight.py
@@ -9,11 +9,9 @@ class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
         stones = [-s for s in stones]
         heapq.heapify(stones)
-        while len(stones) >1:
-            first = heapq.heappop(stones)
-            second = heapq.heappop(stones)
-            if second > first:
-                heapq.heappush(stones, first - second)
+        while len(stones) > 1:
+            x,y = heapq.heappop(stones),heapq.heappop(stones)
+            heapq.heappush(stones,x-y)
         stones.append(0)
         return abs(stones[0])
 # @lc code=end
