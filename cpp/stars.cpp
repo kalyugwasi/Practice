2f3f001548db0f3b9d8389149537c435e9a2d74f
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2025-11-19 18:03:31 +0530

    html

---
+++ cpp/stars.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using stream std
+void main(){
+    int delimiter = 5#include <iostream>
+        using namespace std;
+    int main() {
+        int delimiter = 5;
+        for (int i = 0; i < delimiter; i++) {
+            for (int j = 1; j < delimiter; j++) {
+                if (i == 0 or i == delimiter - 1 or j == 1 or j == delimiter) {
+                    cout << "* ";
+                }
+            }
+            cout << endl;
+        }
+        return 0;
+    }
+    for (int i=0;i<delimiter,i++){
+        for (int j=0;j<i,j++){
+            cout>>"*";
+        }
+    }
+}
