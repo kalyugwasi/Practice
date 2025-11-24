de3efa5edc6261ed38791ece2fa576d2d3bd3648
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2025-11-24 23:34:13 +0530

    fix

--- cpp/stars.cpp
+++ cpp/stars.cpp
@@ -1,23 +1,15 @@
 #include<iostream>
-using stream std
-void main(){
-    int delimiter = 5#include <iostream>
-        using namespace std;
-    int main() {
-        int delimiter = 5;
-        for (int i = 0; i < delimiter; i++) {
-            for (int j = 1; j < delimiter; j++) {
-                if (i == 0 or i == delimiter - 1 or j == 1 or j == delimiter) {
-                    cout << "* ";
-                }
+using namespace std;
+
+int main(){
+    int delimiter = 5;
+    for (int i = 0; i < delimiter; i++) {
+        for (int j = 1; j < delimiter; j++) {
+            if (i == 0 || i == delimiter - 1 || j == 1 || j == delimiter) {
+                cout << "* ";
             }
-            cout << endl;
         }
-        return 0;
+        cout << endl;
     }
-    for (int i=0;i<delimiter,i++){
-        for (int j=0;j<i,j++){
-            cout>>"*";
-        }
-    }
+    return 0;
 }
