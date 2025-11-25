55f533ca5b7a2b728bfbe8060f3caf4b390861ba
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2025-11-25 16:16:25 +0530

    ran graphic.h successfully

--- cpp/circle.cpp
+++ cpp/circle.cpp
@@ -3,8 +3,8 @@
 
 int main() {
     int gd = DETECT, gm;
-    initgraph(&gd, &gm, (char*)"");
-    circle(200, 200, 100);
+    initgraph(&gd, &gm, "");
+    circle(200, 300, 100);
     getch();
     closegraph();
     return 0;
