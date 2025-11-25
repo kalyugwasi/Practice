d12fdc0e37ee86c2d33ee1ea77cc28a6029581d3
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2025-11-25 12:16:36 +0530

    all fine

---
+++ cpp/insertion.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,arr[100];
+    cout<<"Enter number of elements: ";
+    cin>>n;
+    cout<<"Enter "<<n<<" Numbers: ";
+    for (int i=1;i<n;i++){
+        cin>>arr[i];
+    }
+    for (int i=1;i<n;i++){
+        int j=i;
+        while(j>0 && arr[j-1]>arr[j]){
+            swap(arr[j],arr[j-1]);
+            j--;
+        }
+    }
+    cout<<"Sorted numbers: ";
+    for (int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
