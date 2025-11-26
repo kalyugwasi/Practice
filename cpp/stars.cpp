700293d2fddda31532afbb658dbd5ab0e19ad617
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2025-11-26 10:30:48 +0530

    intermediate

--- cpp/stars.cpp
+++ cpp/stars.cpp
@@ -6,14 +6,12 @@ int main() {
 
     for (int i = 0; i < delimiter; i++) {
         for (int j = 0; j < delimiter; j++) {
-
             if (i == 0 || i == delimiter - 1 || j == 0 || j == delimiter - 1) {
                 cout << "* ";
             } else{
                 cout<<"  ";
             }
         }
-        
         cout << endl;
     }
     return 0;
