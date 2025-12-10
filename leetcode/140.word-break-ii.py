d1109150c685bed41fd340d365dd709e9781c12c
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2025-12-11 00:49:18 +0530

    word break 2

--- leetcode/140.word-break-ii.py
+++ leetcode/140.word-break-ii.py
@@ -7,7 +7,24 @@
 # @lc code=start
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
-        return None
-        
+        n=len(s)
+        def bt(i):
+            if i == n:
+                return [""]
+            res = []
+            for j in range(i,n):
+                word = s[i:j+1]
+                if word not in wordDict:
+                    continue
+                strings = bt(j+1)
+                if not strings:
+                    continue
+                for sub in strings:
+                    sentence = word
+                    if sub:
+                        sentence += " " + sub
+                    res.append(sentence)
+            return res
+        return bt(0)
 # @lc code=end
 
