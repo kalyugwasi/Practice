bfc813473b8cf48eda8716458ba2d863a7844e8a
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2026-01-13 23:33:42 +0530

    cpp test + lc

---
+++ cpp/normal/test.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int a = 123, b = 678578789456;
+    string x ="monkey",s;
+    cout << a << " " << b << " " << "\n" << x;
+    getline(cin,s);
+}
---
+++ leetcode/1365.how-many-numbers-are-smaller-than-the-current-number.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=1365 lang=python3
+#
+# [1365] How Many Numbers Are Smaller Than the Current Number
+#
+
+# @lc code=start
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        new = sorted(nums)
+        res = {}
+        for i,n in enumerate(new):
+            if n not in res:
+                res[n] = i
+        return [res[n] for n in nums]
+            
+
+# @lc code=end
+
