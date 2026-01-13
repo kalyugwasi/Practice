e61be7aad21e70d0b0600eee9818c8e611f7320e
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2026-01-13 19:33:34 +0530

    cpp

--- cpp/normal/vertexcover.cpp
+++ cpp/normal/vertexcover.cpp
@@ -1,44 +1,10 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-
-#define MAX 100
-
-int main() {
-    int n, m;
-    cout << "Enter number of vertices and edges: ";
-    cin >> n >> m;
-    
-    int adj[MAX][MAX] = {0};
-    int edges[MAX][2];
-    
-    cout << "Enter edges (u v):\n";
-    for (int i = 0; i < m; i++) {
-        cin >> edges[i][0] >> edges[i][1];
-        adj[edges[i][0]][edges[i][1]] = 1;
-        adj[edges[i][1]][edges[i][0]] = 1;
-    }
-    
-    // Greedy approach for vertex cover
-    bool covered[MAX] = {false};
-    int vertexCover[MAX];
-    int coverSize = 0;
-    
-    for (int i = 0; i < m; i++) {
-        int u = edges[i][0];
-        int v = edges[i][1];
-        
-        if (!covered[u] && !covered[v]) {
-            vertexCover[coverSize++] = u;
-            covered[u] = true;
-            covered[v] = true;
-        }
-    }
-    
-    cout << "\nVertex Cover: ";
-    for (int i = 0; i < coverSize; i++) {
-        cout << vertexCover[i] << " ";
-    }
-    cout << "\nSize: " << coverSize << endl;
-    
-    return 0;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int a = 123, b = 456;
+    string x ="monkey",s;
+    cout << a << " " << b << " " << "\n" << x;
+    getline(cin,s);
 }
