d3190cd6620232ae5b156f7d8afc450d71b8de31
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2026-01-14 01:24:22 +0530

    cp shuffling

---
+++ cp/c++/test.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    
+    double x = 0.3*3+0.1;
+    printf("%.55f\n",x);
+    return 0;
+}
--- cpp/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    long long int a = 1423, b = 57856;
-    string x ="monkey";
-    cout << a << " " << b << " " << "\n" << x;
-}
