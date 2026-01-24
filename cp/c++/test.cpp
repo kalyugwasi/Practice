b1bcd6a61e646118fb8321ba92b13440012745f8
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2026-01-24 11:30:10 +0530

    cp

--- cp/c++/test.cpp
+++ cp/c++/test.cpp
@@ -1,45 +1,31 @@
+#define PB push_back
+#define PF push_front
+#define PopB pop_back
+#define PopF pop_front
 #include <bits/stdc++.h>
 using namespace std;
+typedef deque<int> di;
 
-const int INF = 1e9;
-
-int solve(int x, vector<int>& coins, vector<int>& dp) {
-    if (x < 0) return INF;
-    if (x == 0) return 0;
-    if (dp[x] != -1) return dp[x];
-
-    int best = INF;
-    for (int c : coins) {
-        best = min(best, solve(x - c, coins, dp) + 1);
-    }
-
-    return dp[x] = best;
+void print(const deque<int>& d) {
+        cout<<"[";
+        for (int x : d) cout <<x << ' ';
+        cout << "]"<<'\n';
 }
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    freopen("input.txt", "r", stdin);
+    freopen("input.txt","r",stdin);
     freopen("output.txt", "w", stdout);
-
-    int target, n;
-    cin >> target >> n;
-
-    vector<int> coins(n);
-    for (int i = 0; i < n; i++) {
-        cin >> coins[i];
+    di d;
+    int x;
+    while (cin >> x) {
+        d.push_back(x);
     }
-
-    vector<int> dp(target + 1, -1);
-    int ans = solve(target, coins, dp);
-
-    if (ans == INF)
-        cout << -1;
-    else
-        cout << ans <<"\n";
-    for (auto i:dp){
-        cout<<i<<"\n";
-    }
-
+    cout<<d.size();
+    d.PB(2); print(d);
+    d.PB(2); print(d);
+    d.PF(3); print(d);
+    d.PopB(); print(d);
+    d.PopF(); print(d);
     return 0;
 }
