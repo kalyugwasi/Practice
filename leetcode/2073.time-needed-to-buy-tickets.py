fb58c136a9fb96b16c8320d309afd5225647a347
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2026-01-25 19:42:09 +0530

    optimized

--- leetcode/2073.time-needed-to-buy-tickets.py
+++ leetcode/2073.time-needed-to-buy-tickets.py
@@ -8,12 +8,15 @@
 class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
         time = 0
-        for i in range(len(tickets)):
-            if i <= k:
-                time += min(tickets[i], tickets[k])
-            else:
-                time += min(tickets[i], tickets[k] - 1)
+        queue = deque(range(len(tickets)))
+        while queue:
+            time += 1
+            q = queue.popleft()
+            tickets[q] -= 1
+            if tickets[q] == 0 and k == q:
+                return time
+            if tickets[q] >0:
+                queue.append(q)
         return time
-        
 # @lc code=end
 
