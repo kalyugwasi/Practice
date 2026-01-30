0a37386cfbf6304236acb36567c6cbabde649dde
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2026-01-30 14:52:30 +0530

    cp

---
+++ cp/python/problems/800/1869A.py
@@ -0,0 +1,53 @@
+import sys,os
+def setup_io():
+    try:
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        judge_dir = os.path.join(base_dir, "..", "judge")
+
+        input_path = os.path.join(judge_dir, "input.txt")
+        output_path = os.path.join(judge_dir, "output.txt")
+
+        if os.path.exists(input_path):
+            sys.stdin = open(input_path, "r")
+            sys.stdout = open(output_path, "w")
+            return True
+    except:
+        pass
+
+    return False
+LOCAL = setup_io()
+input = sys.stdin.readline
+int1 = lambda: int(input())
+str1 = lambda: input().strip()
+def inp(): return map(int, input().split())
+def ints(): return list(map(int, input().split()))
+def strs(): return list(map(str,str1()))
+
+# ================== SOLUTION START ==================
+
+t = int1()
+for _ in range(t):
+    n = int1()
+    a = ints()
+    if all(x == 0 for x in a):
+        print(0)
+        continue
+
+    if n % 2 == 0:
+        print(2)
+        print(1, n)
+        print(1, n)
+    else:
+        print(4)
+        print(1, n-1)
+        print(1, n-1)
+        print(n-1, n)
+        print(n-1, n)    
+          
+
+# ================== SOLUTION END ==================
+
+if LOCAL:
+    sys.stdout.flush()
+#cfjudge
+#python run.py
