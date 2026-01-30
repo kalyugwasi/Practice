efea5b464e2324159628f17a79e77bbf21c99247
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2026-01-31 00:45:47 +0530

    cp

--- cp/python/test.py
+++ cp/python/test.py
@@ -27,18 +27,9 @@ def strs(): return list(map(str,str1()))
 
 t = int1()
 for _ in range(t):
-    n,q = inp()
+    n = int1()
     a = ints()
-    olds = sum(a)
-    prefix = [0] * (n+1)
-    for i in range(1,n+1):
-        prefix[i] = prefix[i-1] + a[i-1]
-    for __ in range(q):
-        l,r,k = inp()
-        sumr = prefix[r] - prefix[l-1]
-        suma = (r-l+1) * k
-        total = olds - sumr + suma
-        print("YES" if total%2!=0 else "NO")        
+    
 
 # ================== SOLUTION END ==================
 
