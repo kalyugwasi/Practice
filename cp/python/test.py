194046bca0c3c500ff7af6a1d9eb5afc32091ae6
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2026-02-22 23:03:24 +0530

    cf

--- cp/python/test.py
+++ cp/python/test.py
@@ -30,6 +30,28 @@ t = int1()
 for _ in range(t):
     n = int1()
     a = ints()
+    i,j = 0,n-1
+    low,high = 1,n
+    while i<j:
+        if a[i] == low or a[i]== high:
+            if a[i] == low:
+                low += 1
+            else:
+                high -= 1
+            i += 1
+            continue
+        if a[j] == low or a[j] == high:
+            if a[j] == low:
+                low += 1
+            else:
+                high -= 1
+            j -= 1
+            continue
+        break
+    if i<j:
+        print(i+1,j-1)
+    else:
+        print(-1)
     
 
 # ================== SOLUTION END ==================
