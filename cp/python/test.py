434dba95b0438fbbc1bc9e456472321a6ec88f7e
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2026-03-01 02:40:41 +0530

    vim

--- cp/python/test.py
+++ cp/python/test.py
@@ -1,4 +1,82 @@
-def printer(m,table):
+def canConstruct():
+
+print(canConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"]))
+print(canConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"]))
+print(canConstruct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"]))
+print(canConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", ["e","ee","eee","eeee","eeeee","eeeeee"]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+def bestsum(target,numbers):
+    table = [None]*(target+1)
+    table[0] = []
+    for i in range(target+1):
+        if table[i] is not None:
+            for num in numbers:
+                comb = table[i] + [num]
+                if num+i <= target and (not table[num+i] or len(comb)<len(table[num+i])):
+                    table[num+i] = comb
+    return table[target]
+
+print(bestsum(7,[5,3,4]))
+print(bestsum(8,[2,3,5]))
+print(bestsum(300,[7,14]))
+print(bestsum(7,[2,3]))
+print(bestsum(7,[2,4]))
+print(bestsum(100,[25,1,2,3]))
+print(bestsum(100,[1,2,3,25]))
+print(bestsum(7,[5,3,7,4]))
+"""
+"""
+def howsum(target,numbers):
+    table = [None]*(target+1)
+    table[0] = []
+    for i in range(target+1):
+        if table[i] is not None:
+            for num in numbers:
+                if num+i <= target:
+                    table[num+i] = table[i] + [num]
+    return table[target]
+
+print(howsum(7,[5,3,4]))
+print(howsum(8,[2,3,5]))
+print(howsum(300,[7,14]))
+print(howsum(7,[2,3]))
+print(howsum(7,[2,4]))
+print(howsum(7,[5,3,7,4]))
+"""
+
+
+"""
+def cansum(target,numbers):
+    table = [False]*(target+1)
+    table[0] = True
+    for i in range(target):
+        if table[i]:
+            for num in numbers:
+                if num+i <= target: table[num+i] = True
+    return table[target] 
+
+print(cansum(7,[5,3,4,7]))
+print(cansum(7,[2,3]))
+print(cansum(7,[2,4]))
+print(cansum(8,[2,3,5]))
+print(cansum(300,[7,14]))
+"""
+
+
+"""def printer(m,table):
     for i in range(m):
         print(*table[i],sep="   ")
 def grid(m,n):
@@ -15,4 +93,5 @@ print(grid(1,1))
 print(grid(3,3))
 print(grid(6,6))
 print(grid(18,18))
-print(grid(20,20))
+print(grid(30,30))
+"""
