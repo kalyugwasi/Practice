a0ade73b2c092b04b2e4c385d42a665ac6be6b0a
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2026-03-13 01:57:29 +0530

    restructuring

--- cp/c++/test.cpp
+++ cp/c++/test.cpp
@@ -6,26 +6,18 @@
 using namespace std;
 typedef deque<int> di;
 
-void print(const deque<int>& d) {
-        cout<<"[";
-        for (int x : d) cout <<x << ' ';
-        cout << "]"<<'\n';
+int hamming(string a, string b){
+    int d = 0
+    for (int i=0;i<k,i++){
+        if (a[i]!=b[i]) d++;
+    }
+    return d
 }
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    freopen("input.txt","r",stdin);
+    //freopen("input.txt","r",stdin);
     freopen("output.txt", "w", stdout);
-    di d;
-    int x;
-    while (cin >> x) {
-        d.push_back(x);
-    }
-    cout<<d.size();
-    d.PB(2); print(d);
-    d.PB(2); print(d);
-    d.PF(3); print(d);
-    d.PopB(); print(d);
-    d.PopF(); print(d);
+    cout<<hamming(00111,)
     return 0;
 }
--- leetcode/tester.py
+++ leetcode/tester.py
@@ -1,20 +1,9 @@
-import re
-def count_tokens(code):
-    token_pattern = (
-        r'"[^*]*"|'      #checks all ""     
-        r'\b\d+\b|'      #checks all numbers  
-        r'\b[a-zA-Z_]\w*\b|' #checks all words
-        r'[+\-*/%=;]'     #checks all symbols
-    )
-    tokens = re.findall(token_pattern, code)
-    return len(tokens), tokens
+a = {1,3,4,8}
+b = {3,6,8,9}
 
-fode = "x=y+3;"
-code = 'x="y+3";'
-
-count, token_list = count_tokens(fode)
-print("Tokens:", token_list," =", count)
-count, token_list = count_tokens(code)
-print("Tokens:", token_list," =", count)
-
-
+print(a & b)   # intersection
+print(a | b)   # union
+print(a - b)   # difference
+n = 5
+for i in range(0,n):
+    print((i<<n))
