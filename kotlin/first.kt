d8ee30f2968dcfbe4f433e15597c73997b97f99b
Himanshu Kumar <108016685+kalyugwasi@users.noreply.github.com>
2026-06-11 00:10:37 +0530

    till loops

--- kotlin/first.kt
+++ kotlin/first.kt
@@ -1,16 +1,42 @@
-import java.util.*
-fun calculate(ha:String,a:Int,b:Int):Int{
-    if (ha == "add"){
-        return a+b
-    }
-    return 0
-}
+import java.util.*    
+// fun main(){
+//     var animal = "horse"
+//     when(animal){
+//         "horse" -> println("Animal is Horse")
+//         "dog" -> println("Animal is dog")
+//         "cat" -> println("Animal is cat")
+//         else -> println("unknown animal")
+//     }
+// }
+// fun main(){
+//     var scan = Scanner(System.`in`)
+//     println("Enter the percentage you got")
+//     var percentage = scan.nextInt()
+//     when(percentage){
+//         in 90..100 -> println("He is a fucking topper")
+//         in 80..90 -> println("was just about to be a topper")
+//         in 50..80 -> println("you are an average dude")
+//         in 35..50 -> println("god save you dude")
+//         else -> println("dont come back again you failure")
+//     }
+// }
+// fun main(){
+//     var number: Int = 2689
+//     var index: Int = 1
+//     while(index<=10){
+//         println("$number * $index = ${number*index}")
+//         index++
+//     }
+// }
+// fun main(){
+//     var count = 0
+//     for (i in 1..10 step 2){
+//         println("hello $i")
+//     }
+//     for (i in 10 downTo 1 step 2){
+//         println(i)
+//     }
+// }
 fun main(){
-    val scanner = Scanner(System.`in`)
-    println("enter first value: ")
-    var a = scanner.nextInt()
-    println("enter second value: ")
-    var b = scanner.nextInt()
-    val ha = "add"
-    println(calculate(ha,a,b))
+    
 }
